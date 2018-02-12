b1dea89f90788f30469dddc74c7732c24ae4e47e
nabab <thomas.nabet@gmail.com>
2018-02-12 13:29:39 +0100

    is_admin to is_dev

--- src/mvc/public/list.php
+++ src/mvc/public/list.php
@@ -5,7 +5,7 @@ if ( !empty($ctrl->arguments) ){
 }
 if ( empty($ctrl->post) ){
   echo $ctrl->combo(_("Tâches automatisées (CRON)"), [
-    'is_dev' => $ctrl->inc->user->is_admin(),
+    'is_dev' => $ctrl->inc->user->is_dev(),
     'root' => $ctrl->data['root'],
     'can_run' => $ctrl->inc->perm->has($ctrl->data['root'].'run'),
     'lng' => [
