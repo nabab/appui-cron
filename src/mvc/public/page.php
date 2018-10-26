51d555d85f939bcba8e9f2c31f0458fad0f70e4a
nabab <thomas.nabet@gmail.com>
2018-10-26 19:07:22 +0200

    Translations

--- src/mvc/public/page.php
+++ src/mvc/public/page.php
@@ -5,7 +5,9 @@
  **/
 
 /** @var $this \bbn\mvc\controller */
-$ctrl->obj->url = $ctrl->data['root'].'page';
-$ctrl->set_icon('far fa-clock')
-  ->set_color('brown', '#FFF')
-  ->combo(_('Automatized tasks'));
+if ( strpos($ctrl->baseURL, $ctrl->data['root'] . 'page/') !== 0 ){
+  $ctrl->obj->url = $ctrl->data['root'].'page';
+  $ctrl->set_icon('far fa-clock')
+       ->set_color('brown', '#FFF')
+       ->combo(_('Automatized tasks'));
+}
