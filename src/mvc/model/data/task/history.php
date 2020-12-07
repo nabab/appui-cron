85dde75babae6330326e2195577110f8e53b2caa
Thomas Nabet <thomas.nabet@gmail.com>
2020-12-07 01:22:16 +0100

    Fixed error with jobs with no frequency

--- src/mvc/model/data/task/history.php
+++ src/mvc/model/data/task/history.php
@@ -16,16 +16,24 @@ if ($model->has_var('id', true) && $model->inc->cron->check()) {
       unset($model->data['id']);
     }
   }
+
   $data = $model->inc->cron->get_day_logs($model->data);
   $task = [];
-  if ( !empty($model->data['id']) ){
+  if (!empty($model->data['id'])) {
     $task = $model->inc->cron->get_manager()->get_cron($model->data['id']);
-    if ( is_array($task['cfg']) ){
+    if (is_array($task['cfg'])) {
       $task = array_merge($task, $task['cfg']);
       unset($task['cfg']);
     }
-    $task['next'] = $model->inc->cron->get_manager()->get_next_date($task['frequency'], strtotime($task['next'] ?: $task['prev']));
+
+    if (!empty($task['frequency'])) {
+      $task['next'] = $model->inc->cron->get_manager()->get_next_date(
+        $task['frequency'],
+        strtotime($task['next'] ?: $task['prev'])
+      );
+    }
   }
+
   return [
     'data' => $data,
     'total' => count($data),
@@ -34,6 +42,7 @@ if ($model->has_var('id', true) && $model->inc->cron->check()) {
     'success' => true
   ];
 }
+
 return [
   'success' => false
 ];
--- src/mvc/model/data/task/info.php
+++ src/mvc/model/data/task/info.php
@@ -1,14 +1,20 @@
 <?php
-if (
-  $model->has_var('id', true) &&
-  \bbn\str::is_uid($model->data['id']) &&
-  ($task = $model->inc->cron->get_manager()->get_cron($model->data['id']))
-){
-  if ( is_array($task['cfg']) ){
+if ($model->has_data('id', true)
+    && \bbn\str::is_uid($model->data['id'])
+    && ($task = $model->inc->cron->get_manager()->get_cron($model->data['id']))
+) {
+  if (is_array($task['cfg'])) {
     $task = array_merge($task, $task['cfg']);
     unset($task['cfg']);
   }
-  $task['next'] = $model->inc->cron->get_manager()->get_next_date($task['frequency'], strtotime($task['next'] ?: $task['prev']));
+
+  if (!empty($task['frequency'])) {
+    $task['next'] = $model->inc->cron->get_manager()->get_next_date(
+      $task['frequency'],
+      strtotime($task['next'] ?: $task['prev'])
+    );
+  }
+
   return [
     'success' => true,
     'task' => $task
