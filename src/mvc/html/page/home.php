2cadc12e0612e1d65c08409faa07baf62b5f4c0c
Thomas Nabet <thomas.nabet@gmail.com>
2021-03-21 13:43:45 +0100

    Fix in HTML structure

--- src/mvc/html/page/home.php
+++ src/mvc/html/page/home.php
@@ -70,80 +70,82 @@
       </div>
     </bbn-toolbar>
     <div class="bbn-flex-fill">
-      <bbn-splitter orientation="horizontal"
-                    class="appui-cron-main-container"
-                    :resizable="true"
-                    :collapsible="true"
-      >
-        <bbn-pane :size="250"
-                  class="appui-cron-pane-list"
-                  :scrollable="true"
+      <div class="bbn-overlay">
+        <bbn-splitter orientation="horizontal"
+                      class="appui-cron-main-container"
+                      :resizable="true"
+                      :collapsible="true"
         >
-          <div class="bbn-w-100 bbn-bottom-space">
-            <div v-text="_('Current task')"
-                 class="bbn-c bbn-header bbn-b bbn-large bbn-no-border-top bbn-no-border-right"
-            ></div>
-            <div class="bbn-w-100 bbn-c bbn-spadded">
-              <bbn-dropdown :source="source.quicklist"
-                            v-model="currentTask"
-                            :placeholder="_('Pick a task')"
-                            @change="selectByDD"
-                            class="bbn-w-100"
-              ></bbn-dropdown>
-            </div>
-          </div>
-          <div class="bbn-w-100">
-            <div v-text="_('Running tasks')"
-                 class="bbn-b bbn-c bbn-header bbn-large bbn-no-border-right"
-            ></div>
-            <bbn-list :source="tasksList"
-                      :component="$options.components['activeTasksItem']"
-                      @select="select1"
-                      ref="list1"
-                      source-value="id"
-                      uid="file"
-                      class="bbn-vmargin"
-            ></bbn-list>
-          </div>
-          <div class="bbn-w-100">
-            <div v-text="_('Failed tasks')"
-                 class="bbn-b bbn-large bbn-c bbn-header bbn-no-border-right"
-            ></div>
-            <bbn-list :source="source.failed"
-                      :component="$options.components['failedItem']"
-                      ref="list3"
-                      source-value="id"
-                      source-text="file"
-                      uid="file"
-                      @select="select3"
-                      class="bbn-vmargin"
-            ></bbn-list>
-          </div>
-          <div class="bbn-w-100">
-            <div v-text="_('Coming tasks')"
-                 class="bbn-c bbn-header bbn-b bbn-large bbn-no-border-right"
-            ></div>
-            <bbn-list :source="source.tasks"
-                      :component="$options.components['tasksItem']"
-                      @select="select2"
-                      ref="list2"
-                      source-value="id"
-                      uid="file"
-                      class="bbn-vmargin"
-            ></bbn-list>
-          </div>
-        </bbn-pane>
-        <bbn-pane>
-          <appui-cron-task v-if="currentTask && showTask"
-                           :source="taskSource"
-          ></appui-cron-task>
-          <div v-else
-               class="bbn-overlay bbn-middle"
+          <bbn-pane :size="250"
+                    class="appui-cron-pane-list"
+                    :scrollable="true"
           >
-            <span class="bbn-xl bbn-b"><i class="nf nf-fa-arrow_left bbn-right-space"></i><?=_('Select a task')?></span>
-          </div>
-        </bbn-pane>
-      </bbn-splitter>
+            <div class="bbn-w-100 bbn-bottom-space">
+              <div v-text="_('Current task')"
+                  class="bbn-c bbn-header bbn-b bbn-large bbn-no-border-top bbn-no-border-right"
+              ></div>
+              <div class="bbn-w-100 bbn-c bbn-spadded">
+                <bbn-dropdown :source="source.quicklist"
+                              v-model="currentTask"
+                              :placeholder="_('Pick a task')"
+                              @change="selectByDD"
+                              class="bbn-w-100"
+                ></bbn-dropdown>
+              </div>
+            </div>
+            <div class="bbn-w-100">
+              <div v-text="_('Running tasks')"
+                  class="bbn-b bbn-c bbn-header bbn-large bbn-no-border-right"
+              ></div>
+              <bbn-list :source="tasksList"
+                        :component="$options.components['activeTasksItem']"
+                        @select="select1"
+                        ref="list1"
+                        source-value="id"
+                        uid="file"
+                        class="bbn-vmargin"
+              ></bbn-list>
+            </div>
+            <div class="bbn-w-100">
+              <div v-text="_('Failed tasks')"
+                  class="bbn-b bbn-large bbn-c bbn-header bbn-no-border-right"
+              ></div>
+              <bbn-list :source="source.failed"
+                        :component="$options.components['failedItem']"
+                        ref="list3"
+                        source-value="id"
+                        source-text="file"
+                        uid="file"
+                        @select="select3"
+                        class="bbn-vmargin"
+              ></bbn-list>
+            </div>
+            <div class="bbn-w-100">
+              <div v-text="_('Coming tasks')"
+                  class="bbn-c bbn-header bbn-b bbn-large bbn-no-border-right"
+              ></div>
+              <bbn-list :source="source.tasks"
+                        :component="$options.components['tasksItem']"
+                        @select="select2"
+                        ref="list2"
+                        source-value="id"
+                        uid="file"
+                        class="bbn-vmargin"
+              ></bbn-list>
+            </div>
+          </bbn-pane>
+          <bbn-pane>
+            <appui-cron-task v-if="currentTask && showTask"
+                            :source="taskSource"
+            ></appui-cron-task>
+            <div v-else
+                class="bbn-overlay bbn-middle"
+            >
+              <span class="bbn-xl bbn-b"><i class="nf nf-fa-arrow_left bbn-right-space"></i><?=_('Select a task')?></span>
+            </div>
+          </bbn-pane>
+        </bbn-splitter>
+      </div>
     </div>
   </div>
 </div>
