2c0754fce052d1419ac88e0da930fcbd1fd229ee
nabab <thomas.nabet@gmail.com>
2022-01-07 05:38:44 +0100

    Fix in addInc

--- src/mvc/public/_ctrl.php
+++ src/mvc/public/_ctrl.php
@@ -4,9 +4,13 @@
 if ( !\defined('APPUI_CRON_ROOT') ){
   define('APPUI_CRON_ROOT', $ctrl->pluginUrl('appui-cron').'/');
 }
+
 $ctrl->addData([
   'root'=> APPUI_CRON_ROOT,
   'data_path' => $ctrl->pluginDataPath()
 ]);
-$ctrl->addInc('cron', (new \bbn\Cron($ctrl->db, $ctrl)));
+if (empty($ctrl->inc->cron)) {
+  $ctrl->addInc('cron', (new \bbn\Cron($ctrl->db, $ctrl)));
+}
+
 return true;
