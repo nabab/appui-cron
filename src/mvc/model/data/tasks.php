1e6565394126a02a1cc4836708d50364a748f5ac
Mirko Argentino <mirko@bbn.solutions>
2025-04-10 10:51:28 +0200

    Removed unnecessary code

--- src/mvc/model/data/tasks.php
+++ src/mvc/model/data/tasks.php
@@ -1,20 +1,13 @@
 <?php
-/*
- * Describe what it does!
- *
- **/
-
 /** @var $this \bbn\Mvc\Model*/
-$r = ['success' => false, 'check' => $model->inc->cron->check()];
-
+$r = [
+  'success' => false,
+  'check' => $model->inc->cron->check()
+];
 if ($model->inc->cron->check()) {
-  $tasks = $model->inc->cron->getManager()->getNextRows(100, 3600);
-  $failed = $model->inc->cron->getManager()->getFailed();
-  $tasks = array_filter($tasks, function($t) use($failed){
-    return \bbn\X::find($failed, ['id' => $t['id']]) === null;
-  });
   $r['success'] = true;
   $r['tasks'] = $model->inc->cron->getManager()->getNextRows(100, 3600);
-  $f['failed'] = $failed;
+  $f['failed'] = $model->inc->cron->getManager()->getFailed();
 }
+
 return $r;
